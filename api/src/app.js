7b626166c1dc844ae3d0b124f6c07c796fcb9a5f
Travis Bumgarner <travis.bumgarner@gmail.com>
2018-11-03 20:31:04 +0000

    (api) Setup to run with slash commands

--- api/src/app.js
+++ api/src/app.js
@@ -1,8 +1,34 @@
 import express from "express"
+import bodyParser from 'body-parser'
 
 const app = express()
 
-app.get("/", (request, response) => response.send("Hello World"))
+app.use(bodyParser())
+
+app.post("/", (request, response) => {
+    const {command, text, user_id, user_name, channel_name, channel_id, team_domain} = request.body
+
+    if(text === "" || text === "help"){
+        response.send('Help is coming...')
+    }
+
+    if(text === "" || text === "help"){
+        response.send('Help is coming...')
+    }
+
+    if(text === "" || text === "help"){
+        response.send('Help is coming...')
+    }
+
+    else{
+        response.send('Invalid command. Try running `/pairme help`')
+    } 
+
+    console.log(request.body)
+    response.send("Hello World")
+ })
+ 
+
 app.get("/ok", (request, response) => response.send("Service is running"))
 
 export default app
