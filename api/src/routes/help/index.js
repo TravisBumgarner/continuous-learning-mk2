fad20498141effbb193d5b79efbfd65c70cc8984
Travis Bumgarner <travis.bumgarner@gmail.com>
2018-11-04 16:09:09 -0500

    (api) Updated help

--- api/src/routes/help/index.js
+++ api/src/routes/help/index.js
@@ -1,16 +1,56 @@
-const ROOT_COMMAND = "/pairme"
-const HELP_COMMANDS = [
-    ["help", "Display this menu"],
-    ["subscribe", "Join Us"],
-    ["unsubscribe", "Leave Us"],
-    ["update", "Update"]
+const formatExample = command => `\`/pairme ${command}\``
+
+// To add commands, either append them to an existing sub group of commands or create a new sub group,
+// and add a new section to HELP_COMMANDS
+
+const GETTING_STARTED_COMMANDS = [
+    {
+        subCommand: "help",
+        message: "Display this menu",
+        example: ""
+    },
+    {
+        subCommand: "subscribe",
+        message: "Enroll in Continuous Learning",
+        example: formatExample("subscribe")
+    },
+    {
+        subCommand: "unsubscribe",
+        message: "Disenroll in Continuous Learning",
+        example: formatExample("unsubscribe")
+    },
+    {
+        subCommand: "my_languages",
+        message:
+            "Specify the languages that you would like to work on, separated by commas. Rerun command to change languages",
+        example: formatExample("my_languages javascript,python")
+    },
+    {
+        subCommand: "available_languages",
+        message: "See available languages",
+        example: formatExample("available_languages")
+    }
 ]
 
-const generateBody = ({}) => {
-    const body = HELP_COMMANDS.map(([subCommand, message]) => {
-        return `\`${ROOT_COMMAND} ${subCommand}\` - ${message}`
+const HELP_COMMANDS = [
+    {
+        section: "Getting Started",
+        content: GETTING_STARTED_COMMANDS
+    }
+]
+
+const formatSectionContent = section => {
+    return section
+        .map(({ subCommand, message, example }) => {
+            return `\`${subCommand}\` - ${message} (ex: ${example})`
+        })
+        .join("\n")
+}
+
+const generateBody = () => {
+    const body = HELP_COMMANDS.map(({ section, content }) => {
+        return `*${section}*\n${formatSectionContent(content)}`
     }).join("\n")
-    
     return {
         text: body
     }
