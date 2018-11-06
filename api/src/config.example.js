b58bc3524131e434f4643b5d17ac60795e6b5360
Travis Bumgarner <travis.bumgarner@gmail.com>
2018-11-06 18:05:38 -0500

    (api) proper exports

--- api/src/config.example.js
+++ api/src/config.example.js
@@ -1,4 +1,4 @@
-export default {
+const config = {
     db: {
         host: "",
         user: "",
@@ -6,3 +6,5 @@ export default {
         database: ""
     }
 }
+
+module.exports = config
--- api/src/db/knexfile.js
+++ api/src/db/knexfile.js
@@ -1,4 +1,4 @@
-import config from "../config"
+const config = require("../config")
 
 const { host, user, password, database } = config.db
 
