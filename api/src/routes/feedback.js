4117f619fa37c16a61d84217610943001153343b
Travis Bumgarner <travis.bumgarner@gmail.com>
2018-11-09 23:49:18 -0500

    (api) Formatted feedback

--- api/src/routes/feedback.js
+++ api/src/routes/feedback.js
@@ -1,4 +1,5 @@
 import { feedback } from "../db"
+import { ATTACHMENT_COLOR } from "../constants"
 
 const generateErrorMessage = (error, user_id) => {
     const ERROR_CODES_DICT = {}
@@ -8,10 +9,20 @@ const generateErrorMessage = (error, user_id) => {
 }
 
 const generateBody = async ({ user_id, user_name, text }) => {
-    return feedback
-        .create({ user_id, user_name, text })
-        .then(response => `Thank you for your feedback!`)
-        .catch(error => generateErrorMessage(error, user_id))
+    const result = await feedback.create({ user_id, user_name, text })
+    return {
+        attachments: [
+            {
+                color: ATTACHMENT_COLOR,
+                fields: [
+                    {
+                        title: "Feedback Received!",
+                        value: "Thank you for your help."
+                    }
+                ]
+            }
+        ]
+    }
 }
 
 export default generateBody
