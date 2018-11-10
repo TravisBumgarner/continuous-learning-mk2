83440cffc1c6282f61e2d8158673e8542afe28ee
Travis Bumgarner <travis.bumgarner@gmail.com>
2018-11-10 00:26:41 -0500

    (api) New pairings send slack message

--- api/src/utilities/sendUserMessage.js
+++ api/src/utilities/sendUserMessage.js
@@ -1,18 +1,24 @@
 import axios from "axios"
 
 import { makeUrl } from "../utilities"
+import { ATTACHMENT_COLOR } from "../constants"
 import { users } from "../db"
 
-const sendUserMessage = (user_id, text) => {
+const sendUserMessage = (user_id, message) => {
     users.getById(user_id).then(user => {
         const { user_id, access_token } = user[0]
         console.log(user_id, access_token)
-        const url = makeUrl("https://slack.com/api/chat.postMessage", {
-            token: access_token,
+        const data = JSON.stringify({
             channel: user_id,
-            text
+            attachments: [{ color: ATTACHMENT_COLOR, title: "Hey!", text: message }]
         })
-        axios.post(url)
+
+        return axios.post("https://slack.com/api/chat.postMessage", data, {
+            headers: {
+                "Content-Type": "application/json",
+                Authorization: `Bearer ${access_token}`
+            }
+        })
     })
 }
 
