7b363c4f79421b6e409ac851b9597a638659c45d
Travis Bumgarner <travis.bumgarner@gmail.com>
2018-11-10 19:46:28 -0500

    (api) Removed register

--- ui/src/views/Register/Register.styles.js
+++
@@ -1,9 +0,0 @@
-import Styled from 'styled-components'
-
-const RegisterWrapper = Styled.div`
-    width: 500px;
-    height: 500px;
-    margin: calc(50vh - 250px) auto 0;
-`
-
-export { RegisterWrapper }
--- ui/src/views/Register/components/index.js
+++
@@ -1,4 +0,0 @@
-import SelectSkills from './SelectSkills/SelectSkills'
-import RateSkill from './RateSkill/RateSkill'
-
-export { RateSkill, SelectSkills }
